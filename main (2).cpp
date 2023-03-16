4c9c60245baa7ebe1ba8dd9a848d775297b618c7
Lukmannh21 <114939410+Lukmannh21@users.noreply.github.com>
2023-03-16 15:09:12 +0700

    tes
    tes34
    sfas
    sdf

---
+++ main (2).cpp
@@ -0,0 +1,25 @@
+/* TUGAS 2
+NIM  : 221401074
+KOM  : C
+NAMA : Lukman Nur Hakim
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+string nama [30];
+int umur;
+double berat;
+bool isCarnovore;
+string aktifitas;
+char p;
+
+ main()
+{
+    cout << "Masukkan Nama Hewan : "; cin >> nama;
+    cout << "Masukkan Umur : "; cin >> umur;
+    cout << "Masukkan berat hewan (Kg) : "; cin >> berat;
+    cout << "Apakah karnivora (y/n) : "; cin >> p;
+
+    return 0;
+}
